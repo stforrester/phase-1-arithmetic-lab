abff82bbede7fcce6c858937114c6550b6cee1c3
Stuart Forrester <stforrester@gmail.com>
2024-11-03 22:46:55 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 22;
+const num4 = 9;
+const mod = num3 % num4;
+
+const max = (1, 2, 3, 20);
